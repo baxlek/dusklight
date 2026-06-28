9c34402a926a73277b430f3a5b62c94dd58185de
baxlek <115663151+baxlek@users.noreply.github.com>
2026-06-27 19:52:02 -0500

    Update calendar time retrieval method
    Replaced OSCalendarTime declaration with OSTicksToCalendarTime function to update calendar time.

--- src/d/d_kankyo.cpp
+++ src/d/d_kankyo.cpp
@@ -1546,7 +1546,8 @@ void dScnKy_env_light_c::setDaytime() {
                     if (dComIfGp_roomControl_getTimePass() && !field_0x130a && temp_r29) {
                         #if TARGET_PC
                         if (dusk::getSettings().game.systemTimeSync) {
-                            OSCalendarTime std::chrono::system_clock::now();
+                            OSCalendarTime calendarTime
+                            OSTicksToCalendarTime(std::chrono::system_clock::now());
 
                             const f32 calendarDaytime = calendarTime.hour * 15.0f +
                                                       calendarTime.min * (15.0f / 60.0f) +
