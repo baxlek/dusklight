2b7f2b90ab960a5db88688e729de9028f91eb80c
baxlek <115663151+baxlek@users.noreply.github.com>
2026-07-07 16:53:28 -0500

    Refactor config save calls to use lowercase method

--- src/dusk/ui/cosmetics.cpp
+++ src/dusk/ui/cosmetics.cpp
@@ -94,7 +94,7 @@ void add_cosmetic_option(Pane& leftPane, Pane& rightPane, const std::string& key
             pane.add_child<StringButton>(StringButton::Props{
                 .key = "Edit Hex Color",
                 .getValue = [&option] {
-                    return option;
+                    return option.getValue();
                 },
                 .setValue = [&option](Rml::String str) {
                     // Make lowercase
@@ -103,7 +103,7 @@ void add_cosmetic_option(Pane& leftPane, Pane& rightPane, const std::string& key
                     }
 
                     option.setValue(str);
-                    config::Save();
+                    config::save();
                 },
                 .maxLength = 6,
             });
@@ -118,7 +118,7 @@ void add_cosmetic_option(Pane& leftPane, Pane& rightPane, const std::string& key
                 if (key.starts_with("Midna's Hair")) {
                     cosmetics::set_all_midna_hair_colors();
                 }
-                config::Save();
+                config::save();
             });
 
             pane.add_button(ControlledButton::Props{
@@ -128,7 +128,7 @@ void add_cosmetic_option(Pane& leftPane, Pane& rightPane, const std::string& key
                 std::uniform_int_distribution dist(0, 0xFFFFFF);
                 std::string hexStr = randomizer::utility::str::intToHex(dist(rd), 6, false);
                 option.setValue(hexStr);
-                config::Save();
+                config::save();
             });
         }
 
@@ -143,7 +143,7 @@ void add_cosmetic_option(Pane& leftPane, Pane& rightPane, const std::string& key
                 if (key.starts_with("Midna's Hair")) {
                     cosmetics::set_all_midna_hair_colors();
                 }
-                config::Save();
+                config::save();
             });
         }
     });
