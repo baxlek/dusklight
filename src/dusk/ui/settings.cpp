10e114ebc0112aed0897638bdce91fba23553b93
baxlek <115663151+baxlek@users.noreply.github.com>
2026-07-09 15:44:44 -0500

    Fix case of save function in settings.cpp

--- src/dusk/ui/settings.cpp
+++ src/dusk/ui/settings.cpp
@@ -1313,7 +1313,7 @@ SettingsWindow::SettingsWindow(bool prelaunch) : mPrelaunch(prelaunch) {
                         .on_pressed([i] {
                             mDoAud_seStartMenu(kSoundItemChange);
                             getSettings().game.walletSizes.setValue(i);
-                            config::Save();
+                            config::save();
                         });
                 }
                 pane.add_rml(R"(
@@ -1393,7 +1393,7 @@ SettingsWindow::SettingsWindow(bool prelaunch) : mPrelaunch(prelaunch) {
                     }).on_pressed([&var] {
                         mDoAud_seStartMenu(kSoundItemChange);
                         var.setValue(!var.getValue());
-                        config::Save();
+                        config::save();
                     });
                 };
 
