341c08cc8cb1edbcae054cf31f98af94b15839c1
baxlek <115663151+baxlek@users.noreply.github.com>
2026-07-09 03:04:06 -0500

    Add files via upload

--- src/d/d_kankyo.cpp
+++ src/d/d_kankyo.cpp
@@ -1593,7 +1593,8 @@ void dScnKy_env_light_c::setDaytime() {
                                 g_env_light.time_change_rate = 0.012f;
                             }
                         }
-                            if ((!strcmp(dComIfGp_getStartStageName(), "F_SP00")) ||   // Ordon Ranch
+                            if ((dusk::getSettings().game.systemTimeSync == false) &&
+							   (!strcmp(dComIfGp_getStartStageName(), "F_SP00")) ||   // Ordon Ranch
                                (!strcmp(dComIfGp_getStartStageName(), "F_SP103")) ||   // Ordon Village
                                (!strcmp(dComIfGp_getStartStageName(), "F_SP104")) ||   // Ordon Spring
                                (!strcmp(dComIfGp_getStartStageName(), "F_SP109")) ||   // Kakariko Village
