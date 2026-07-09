b27eeb05eaf14c55d490ededc4e7b5e334f8db14
baxlek <115663151+baxlek@users.noreply.github.com>
2026-07-08 20:57:11 -0500

    Add files via upload

--- src/d/d_kankyo.cpp
+++ src/d/d_kankyo.cpp
@@ -1542,15 +1542,16 @@ void dScnKy_env_light_c::setDaytime() {
                     if (msg != NULL && msg->mode >= 2) {
                         temp_r29 = false;
                     }
+					
 						#if TARGET_PC
-						if (dusk::getSettings().game.systemTimeSync) {
-							if (dComIfGp_roomControl_getTimePass() && !field_0x130a && temp_r29) ||
+						if (dusk::getSettings().game.systemTimeSync) &&
+							(dComIfGp_roomControl_getTimePass()) && (!field_0x130a) && (temp_r29) ||
 							(!strcmp(dComIfGp_getStartStageName(), "F_SP00")) ||    // Ordon Ranch
 							(!strcmp(dComIfGp_getStartStageName(), "F_SP103")) ||   // Ordon Village
 							(!strcmp(dComIfGp_getStartStageName(), "F_SP104")) ||   // Ordon Spring
 							(!strcmp(dComIfGp_getStartStageName(), "F_SP109")) ||   // Kakariko Village
 							(!strcmp(dComIfGp_getStartStageName(), "F_SP111")) ||   // Kakariko Graveyard
-							(!strcmp(dComIfGp_getStartStageName(), "F_SP128")) {   // Hidden Village
+							(!strcmp(dComIfGp_getStartStageName(), "F_SP128")) {    // Hidden Village
 
                      //     For when OSGetSystemTime() is implemented  
                      //     OSCalendarTime calendarTime;
@@ -1582,7 +1583,7 @@ void dScnKy_env_light_c::setDaytime() {
 							}
 						}
                         if (dusk::getSettings().game.systemTimeSync == false) {
-							if (dComIfGp_roomControl_getTimePass() && !field_0x130a && temp_r29) {
+							if (dComIfGp_roomControl_getTimePass()) && (!field_0x130a) && (temp_r29) {
 								f32 prev = daytime;
 								daytime += time_change_rate;
 
