c74e2ad94a29bc075947a49ceee5633e8a78a7e6
baxlek <115663151+baxlek@users.noreply.github.com>
2026-07-09 02:35:19 -0500

    Add files via upload

--- src/d/d_kankyo.cpp
+++ src/d/d_kankyo.cpp
@@ -1581,7 +1581,7 @@ void dScnKy_env_light_c::setDaytime() {
                             }
                         }
                         if (dusk::getSettings().game.systemTimeSync == false) {
-                            if (dComIfGp_roomControl_getTimePass() && !field_0x130a && temp_r29)) {
+                            if (dComIfGp_roomControl_getTimePass() && !field_0x130a && temp_r29) {
 
                             f32 prev = daytime;
                             daytime += time_change_rate;
