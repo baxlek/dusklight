f3871dd7358b4920913fde64d5010a7f24f8a3cb
baxlek <115663151+baxlek@users.noreply.github.com>
2026-07-11 23:29:12 -0500

    Refactor YAML embed macros for Battery support
    Add compile-time checks for Battery embed availability and update macros accordingly.

--- src/dusk/randomizer/generator/utility/yaml.hpp
+++ src/dusk/randomizer/generator/utility/yaml.hpp
@@ -6,10 +6,23 @@
 #include "../utility/path.hpp"
 #include "battery/embed.hpp"
 
-#define GET_EMBED_DATA(path) b::embed<path>().str()
-#define LOAD_EMBED_DATA(data) YAML::Load(data)
+// Prefer Battery embed when available, but allow builds without it.
+#if __has_include("battery/embed.hpp")
+  #include "battery/embed.hpp"
+  #define DUSK_HAS_BATTERY_EMBED 1
+#else
+  #define DUSK_HAS_BATTERY_EMBED 0
+#endif
 
-#define LOAD_EMBED_YAML(path) LOAD_EMBED_DATA(GET_EMBED_DATA(path))
+#if DUSK_HAS_BATTERY_EMBED
+  #define GET_EMBED_DATA(path) b::embed<path>().str()
+  #define LOAD_EMBED_DATA(data) YAML::Load(data)
+  #define LOAD_EMBED_YAML(path) LOAD_EMBED_DATA(GET_EMBED_DATA(path))
+#else
+  // If code still tries to use embedded YAML without Battery available,
+  // fail with a clear compile-time message.
+  #define LOAD_EMBED_YAML(path) static_assert(false, "battery/embed.hpp not available; use LoadYAML(...)")
+#endif
 
 // this wrapper is here to avoid path encoding issues
 // removes any possible path -> string oddities or the need to open the file manually
