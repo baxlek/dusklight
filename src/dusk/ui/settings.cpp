f8365819cc96dea7d627a64db5cbd7a38de2906a
baxlek <115663151+baxlek@users.noreply.github.com>
2026-07-14 03:24:11 -0500

    Remove 'World' section and system time sync cheat
    Removed 'World' section and related cheat option for system time synchronization.

--- src/dusk/ui/settings.cpp
+++ src/dusk/ui/settings.cpp
@@ -1431,10 +1431,6 @@ SettingsWindow::SettingsWindow(bool prelaunch) : mPrelaunch(prelaunch) {
             });
         addCheat("Invincible Enemies", getSettings().game.invincibleEnemies,
             "Prevents enemies from taking damage.");
-
-        leftPane.add_section("World");
-        addCheat("System Time Sync", getSettings().game.systemTimeSync,
-            "Synchronizes in-game time with the system clock.");
     });
 
     add_tab("Interface", [this](Rml::Element* content) {
