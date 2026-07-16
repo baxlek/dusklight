3f95a4bc3806fa9a2216193af3425f6fad517bc9
baxlek <115663151+baxlek@users.noreply.github.com>
2026-07-15 20:46:55 -0500

    Refactor time sync check and update comments
    Rearranged conditions for time synchronization check and added comment for clarity.

--- src/d/d_kankyo.cpp
+++ src/d/d_kankyo.cpp
@@ -1545,16 +1545,17 @@ void dScnKy_env_light_c::setDaytime() {
                     }
 
                 #if TARGET_PC
-                    if (dComIfGp_roomControl_getTimePass() && !field_0x130a && temp_r29 ||
-                       (!strcmp(dComIfGp_getStartStageName(), "F_SP00")) ||  // Ordon Ranch
-                       (!strcmp(dComIfGp_getStartStageName(), "F_SP103")) || // Ordon Village
-                       (!strcmp(dComIfGp_getStartStageName(), "F_SP104")) || // Ordon Spring
-                       (!strcmp(dComIfGp_getStartStageName(), "F_SP109")) || // Kakariko Village
-                       (!strcmp(dComIfGp_getStartStageName(), "F_SP111")) || // Kakariko Graveyard
-                       (!strcmp(dComIfGp_getStartStageName(), "F_SP118")) || // Bulblin Camp
-                       (!strcmp(dComIfGp_getStartStageName(), "F_SP128"))) { // Hidden Village
 
-                        if (dusk::getSettings().game.timeSync) {
+                    if (dusk::getSettings().game.timeSync) {
+                    
+                        if (dComIfGp_roomControl_getTimePass() && !field_0x130a && temp_r29 ||
+                           (!strcmp(dComIfGp_getStartStageName(), "F_SP00")) ||  // Ordon Ranch
+                           (!strcmp(dComIfGp_getStartStageName(), "F_SP103")) || // Ordon Village
+                           (!strcmp(dComIfGp_getStartStageName(), "F_SP104")) || // Ordon Spring
+                           (!strcmp(dComIfGp_getStartStageName(), "F_SP109")) || // Kakariko Village
+                           (!strcmp(dComIfGp_getStartStageName(), "F_SP111")) || // Kakariko Graveyard
+                           (!strcmp(dComIfGp_getStartStageName(), "F_SP118")) || // Bulblin Camp
+                           (!strcmp(dComIfGp_getStartStageName(), "F_SP128"))) { // Hidden Village
 
                          // For when OSGetSystemTime() is implemented  
                          // OSCalendarTime calendarTime;
@@ -1614,7 +1615,7 @@ void dScnKy_env_light_c::setDaytime() {
                                 mDate++;
                                 dKankyo_DayProc();
                             }
-                         // Reset certain stages to default
+                         // Reset certain stages to default. Why is  this not working? 
                             if (!strcmp(dComIfGp_getStartStageName(), "F_SP00") ||  // Ordon Ranch
                                 !strcmp(dComIfGp_getStartStageName(), "F_SP103") || // Ordon Village
                                 !strcmp(dComIfGp_getStartStageName(), "F_SP104") || // Ordon Spring
