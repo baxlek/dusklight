8c243fd23f3c3e84d0d7f4f47c9dd07ed7e29039
baxlek <115663151+baxlek@users.noreply.github.com>
2026-08-16 07:52:37 -0500

    Add LetterboxMode enum and update settings

--- src/dusk/settings.h
+++ src/dusk/settings.h
@@ -67,6 +67,13 @@ enum class MagicArmorMode : u8 {
     COSMETIC = 4,
 };
 
+enum class LetterboxMode : u8 {
+    Off = 0,
+    On = 1,
+    CutsceneOnly = 2,
+    GameplayOnly = 3,
+};
+
 namespace config {
 template <>
 struct ConfigEnumRange<BloomMode> {
@@ -120,6 +127,12 @@ template <>
 struct ConfigEnumRange<MagicArmorMode> {
     static constexpr auto min = MagicArmorMode::NORMAL;
     static constexpr auto max = MagicArmorMode::COSMETIC;
+};
+
+template <>
+struct ConfigEnumRange<LetterboxMode> {
+    static constexpr auto min = LetterboxMode::Off;
+    static constexpr auto max = LetterboxMode::GameplayOnly;
 };
 
 template <>
@@ -212,7 +225,7 @@ struct UserSettings {
         ConfigVar<Resampler> resampler;
         ConfigVar<bool> enableMapBackground;
         ConfigVar<bool> disableCutscenePillarboxing;
-        ConfigVar<bool> disableTargetingLetterbox;
+        ConfigVar<LetterboxMode> disableLetterboxing;
 
         // Audio
         ConfigVar<bool> noLowHpSound;
@@ -350,6 +363,13 @@ struct UserSettings {
 UserSettings& getSettings();
 
 void registerSettings();
+
+inline bool isLetterboxingDisabled(bool inCutscene) {
+    const auto mode = getSettings().game.disableLetterboxing.getValue();
+    return mode == LetterboxMode::On ||
+           (mode == LetterboxMode::CutsceneOnly && inCutscene) ||
+           (mode == LetterboxMode::GameplayOnly && !inCutscene);
+}
 
 // Transient settings
 
