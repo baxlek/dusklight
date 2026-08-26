cb9fc35db3b8b8d7aa416ea5ec8a71c6c31684ef
baxlek <115663151+baxlek@users.noreply.github.com>
2026-08-26 17:39:24 -0500

    Fix formatting and add conditional for dusk

--- src/d/actor/d_a_alink.cpp
+++ src/d/actor/d_a_alink.cpp
@@ -4964,6 +4964,7 @@ int daAlink_c::create() {
                                          current.pos.y + 80.0f,
                                          current.pos.z + cM_scos(shape_angle.y) * 70.0f);
             onNoResetFlg1(FLG1_IS_WOLF);
+    IF_DUSK(})
         } else if (isHorseStart) {
             attention_info.position.y = current.pos.y + 275.0f;
         } else {
