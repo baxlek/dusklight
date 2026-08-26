ef317f4b2a1691aaa1febcdad7232abba2c250c8
baxlek <115663151+baxlek@users.noreply.github.com>
2026-08-26 17:12:33 -0500

    Add conditional transform logic for PC target

--- src/d/actor/d_a_alink.cpp
+++ src/d/actor/d_a_alink.cpp
@@ -4950,6 +4950,16 @@ int daAlink_c::create() {
             || sceneMode == 9
             )
         {
+#if TARGET_PC
+            if (startPoint == -4 && dusk::getSettings().game.disableTransformOnWarp) {
+                if (dComIfGs_getTransformStatus()) {
+                    attention_info.position.set(current.pos.x + cM_ssin(shape_angle.y) * 70.0f,
+                                                 current.pos.y + 80.0f,
+                                                 current.pos.z + cM_scos(shape_angle.y) * 70.0f);
+                    onNoResetFlg1(FLG1_IS_WOLF);
+                }
+            } else {
+#endif
             attention_info.position.set(current.pos.x + cM_ssin(shape_angle.y) * 70.0f,
                                          current.pos.y + 80.0f,
                                          current.pos.z + cM_scos(shape_angle.y) * 70.0f);
@@ -17425,7 +17435,7 @@ int daAlink_c::procCoMetamorphoseInit() {
     field_0x347c = 1.0f;
     mFallVoiceInit = 0;
 
-    if ((checkWolf() && mDemo.getDemoMode() == daPy_demo_c::DEMO_METAMORPHOSE_UNK1_e) ||
+    if (((IF_DUSK(dusk::getSettings().game.disableTransformOnWarp || )(checkWolf())) && mDemo.getDemoMode() == daPy_demo_c::DEMO_METAMORPHOSE_UNK1_e) ||
         (!checkWolf() && mDemo.getDemoMode() == daPy_demo_c::DEMO_METAMORPHOSE_UNK2_e))
     {
         mProcVar1.field_0x300a = 1;
