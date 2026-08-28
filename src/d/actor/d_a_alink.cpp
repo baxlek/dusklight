fd4f3b5da2774cb692a078e078ce4b446aa58de3
baxlek <115663151+baxlek@users.noreply.github.com>
2026-08-27 22:21:10 -0500

    Refactor transform status checks for clarity

--- src/d/actor/d_a_alink.cpp
+++ src/d/actor/d_a_alink.cpp
@@ -4964,9 +4964,9 @@ int daAlink_c::create() {
 #if TARGET_PC
             if (startPoint == -4 && dusk::getSettings().game.disableTransformOnWarp) {
                 if (dComIfGs_getTransformStatus()) {
-              //    attention_info.position.set(current.pos.x + cM_ssin(shape_angle.y) * 70.0f,
-              //                                 current.pos.y + 80.0f,
-              //                                 current.pos.z + cM_scos(shape_angle.y) * 70.0f);
+                    attention_info.position.set(current.pos.x + cM_ssin(shape_angle.y) * 70.0f,
+                                                 current.pos.y + 80.0f,
+                                                 current.pos.z + cM_scos(shape_angle.y) * 70.0f);
                     onNoResetFlg1(FLG1_IS_WOLF);
                 }
             } else {
@@ -17453,7 +17453,7 @@ int daAlink_c::procCoMetamorphoseInit() {
     mFallVoiceInit = 0;
 
 #if TARGET_PC
-    if (((dusk::getSettings().game.disableTransformOnWarp && strlen(dMeter2Info_getWarpStageName())) || (checkWolf())) &&
+    if (((dusk::getSettings().game.disableTransformOnWarp && checkNoResetFlg0(FLG0_UNK_4000)) || (checkWolf())) &&
         mDemo.getDemoMode() == daPy_demo_c::DEMO_METAMORPHOSE_UNK1_e) ||
 #else
     if ((checkWolf() && mDemo.getDemoMode() == daPy_demo_c::DEMO_METAMORPHOSE_UNK1_e) ||
