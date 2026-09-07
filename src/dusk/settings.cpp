d57d7a0fa700f75ed763c9051be6dda57bbaf832
baxlek <115663151+baxlek@users.noreply.github.com>
2026-09-07 16:48:24 -0500

    Add deselectShields setting to user preferences

--- src/dusk/settings.cpp
+++ src/dusk/settings.cpp
@@ -57,6 +57,7 @@ UserSettings g_userSettings = {
         .instantText {"game.instantText", false},
         .sunsSong {"game.sunsSong", false},
         .autoSave {"game.autoSave", false},
+        .deselectShields {"game.deselectShields", false},
         .enhancedMapMenus {"game.enhancedMapMenus", false},
         .aimingReticle {"game.aimingReticle", false},
 
@@ -284,6 +285,7 @@ void registerSettings() {
     Register(g_userSettings.game.instantText);
     Register(g_userSettings.game.sunsSong);
     Register(g_userSettings.game.autoSave);
+    Register(g_userSettings.game.deselectShields);
     Register(g_userSettings.game.enhancedMapMenus);
     Register(g_userSettings.game.aimingReticle);
     Register(g_userSettings.game.enableMirrorMode);
