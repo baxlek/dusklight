17a8b02061926f3936ded631697cc6e1f4b17637
baxlek <115663151+baxlek@users.noreply.github.com>
2026-09-18 11:04:19 -0500

    Update super clawshot settings registration

--- src/dusk/settings.cpp
+++ src/dusk/settings.cpp
@@ -357,7 +357,8 @@ void registerSettings() {
     Register(g_userSettings.game.infiniteRupees);
     Register(g_userSettings.game.enableIndefiniteItemDrops);
     Register(g_userSettings.game.moonJump);
-    Register(g_userSettings.game.superClawshot);
+    Register(g_userSettings.game.superClawshotAttach);
+    Register(g_userSettings.game.superClaeshotLength);
     Register(g_userSettings.game.alwaysGreatspin);
     Register(g_userSettings.game.invincibleEnemies);
     Register(g_userSettings.game.easyQuickSpin);
